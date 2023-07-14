024ec1d058a2bd5d406c76fc47dc85cab5d49e8e
Katie Yan <katieyan@Katies-MacBook-Pro.local>
2023-07-13 22:10:56 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import * as ROUTES from "./constants/routes";
+import Home from "./components/Home";
+import "./App.css";
+import Convert from "./components/Convert";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path={ROUTES.HOME} element={<Home />} />
+      <Route path={ROUTES.CONVERT} element={<Convert />} />
+    </Routes>
   );
 }
 
