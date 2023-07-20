c4db7966c77e6961bafc77d3b96ab357a2eb2b86
Katie Yan <katieyan12345@gmail.com>
2023-07-20 04:18:10 -0500

    implement paginate

--- src/functions/getPlaylists.js
+++ src/functions/getPlaylists.js
@@ -11,26 +11,33 @@ export async function getPlaylists() {
   );
   const data = await response.json();
   const total = data.total;
-  const playlist_ids = getPages(accessToken, total);
+  const playlist_data = paginate(
+    "https://api.spotify.com/v1/me/playlists",
+    total
+  );
+  const items_arr = playlist_data.items;
+  const playlist_ids = items_arr.map((item) => {
+    return { id: item.id, name: item.name };
+  });
   return playlist_ids;
 }
 
-async function getPages(accessToken, total) {
-  let playlist_ids = [];
-  for (let offset = 0; offset < total; offset += 50) {
-    const response = await fetch(
-      `https://api.spotify.com/v1/me/playlists?limit=50&offset=${offset}`,
-      {
-        headers: {
-          Authorization: "Bearer " + accessToken,
-        },
-      }
-    );
-    const data = await response.json();
-    const items_arr = data.items;
-    items_arr.map((item) =>
-      playlist_ids.push({ id: item.id, name: item.name })
-    );
-  }
-  return playlist_ids;
-}
+// async function getPages(accessToken, total) {
+//   let playlist_ids = [];
+//   for (let offset = 0; offset < total; offset += 50) {
+//     const response = await fetch(
+//       `https://api.spotify.com/v1/me/playlists?limit=50&offset=${offset}`,
+//       {
+//         headers: {
+//           Authorization: "Bearer " + accessToken,
+//         },
+//       }
+//     );
+//     const data = await response.json();
+//     const items_arr = data.items;
+//     items_arr.map((item) =>
+//       playlist_ids.push({ id: item.id, name: item.name })
+//     );
+//   }
+//   return playlist_ids;
+// }
